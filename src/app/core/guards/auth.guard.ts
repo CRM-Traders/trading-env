d7dda9cdf0cc990851fb897607afcfe36330a3a4
Lasha Shavgulidze <lasha.shavgulidze@caucasusauto.com>
2025-06-19 17:31:17 +0400

    Feature/Testing-Guards

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { environment } from '../../../environments/environment';
 
 export const authGuard: CanActivateFn = (): boolean | UrlTree => {
   const authService = inject(AuthService);
@@ -10,20 +11,7 @@ export const authGuard: CanActivateFn = (): boolean | UrlTree => {
     return true;
   }
 
-  return router.createUrlTree(['/auth/login'], {
+  return router.createUrlTree([environment.redirectUrl], {
     queryParams: { returnUrl: router.url },
   });
-};
-
-export const roleGuard = (requiredRole: string[]): CanActivateFn => {
-  return (): boolean | UrlTree => {
-    const authService = inject(AuthService);
-    const router = inject(Router);
-
-    if (requiredRole.includes(authService.userRole())) {
-      return true;
-    }
-
-    return router.createUrlTree(['/403']);
-  };
 };
